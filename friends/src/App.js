8a013be4d45a4d9ec8de7b5616d21ba6110efb27
stevekutz <stkutz@gmail.com>
2019-04-01 23:21:33 -0500

    axios verified to load friends data into state

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route
+
+
+} from 'react-router-dom'
+
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      activeFriend: null,
+      friends: [],
+      error: '',
+      errorMSG: 'You need to get out and make some friends ;(  ...',
+
+    }
+  }
+
+  componentDidMount() {
+    console.log("CDM active");
+
+    axios
+      .get('http://localhost:5000/friends')
+      .then(result => {
+        console.log("Dan said to check result, so...", result);
+        this.setState({ friends: result.data});
+
+      })
+      .catch(err => {
+        console.log("YOU got an ERROR!", err);
+        this.setState({error: err});
+
+      })
+
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1> Something </h1>
       </div>
     );
   }
---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,11 @@
+import React fron 'react';
+import {Link} from 'react-router-dom';
+
+const FriendList = (props) => {
+  console.log('FriendList props are ', props);
+
+
+
+};
+
+export default FriendList;
